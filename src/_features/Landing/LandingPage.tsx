c29f5536ad2d9cddf13ba3c2954bf02bb25ba027
Mustafa Ahmed <mostafafikry97@gmail.com>
2026-02-03 21:17:37 +0200

    ✨ feat(MainReviewsSection): finished main reviews section in landing page

--- src/_features/Landing/LandingPage.tsx
+++ src/_features/Landing/LandingPage.tsx
@@ -4,6 +4,7 @@ import MetricsSection from "./_subComponents/Metrics/MetricsSection";
 import AllInOneSection from "./_subComponents/AllInOne/AllInOneSection";
 import AboutPlatformSection from "./_subComponents/AboutPlatform/AboutPlatformSection";
 import FeaturesSection from "./_subComponents/Features/FeaturesSection";
+import MainReviewsSection from "./_subComponents/MainReviews/MainReviewsSection";
 
 function LandingPage() {
   return (
@@ -14,6 +15,7 @@ function LandingPage() {
         <AllInOneSection />
         <AboutPlatformSection />
         <FeaturesSection />
+        <MainReviewsSection />
       </div>
     </PageWrapper>
   );
---
+++ src/_features/Landing/_subComponents/MainReviews/MainReviewsSection.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/_components/ui/button";
+import Image from "next/image";
+
+function MainReviewsSection() {
+  return (
+    <div className="px-8">
+      <div className="flex flex-nowrap items-center justify-center gap-y-10 px-4 sm:flex-wrap sm:gap-x-16 sm:px-10 md:gap-x-30 md:px-14 lg:px-20">
+        <div className="flex flex-col gap-6">
+          <p className="testimonial flex items-center gap-2 text-xs">
+            TESTIMONIAL
+          </p>
+          <h3 className="text-prefix text-2xl font-bold">What They Say ?</h3>
+          <p className="text-base">
+            TOTC has got more than 100k positive ratings from our users around
+            the world.
+          </p>
+          <p>
+            Some of the students and teachers were greatly helped by the
+            Skilline.
+          </p>
+          <p>Are you too? Please give your assessment</p>
+          <Button className="w-fit cursor-pointer rounded-full border-2 border-[#49BBBD] bg-transparent px-4 py-1.5 text-[#49BBBD] hover:text-white">
+            Write your assessment
+          </Button>
+        </div>
+        <Image
+          src={
+            "https://res.cloudinary.com/dvvmu40wx/image/upload/v1770144673/samples/eLearning/Review_i6r5kr.png"
+          }
+          width={400}
+          height={400}
+          alt="Review"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MainReviewsSection;
