9efcc3953418eaa7f621eef7cb0a269622def0a4
Mostafa Ahmed <mostafafikry97@gmail.com>
2026-04-06 21:38:13 +0200

    ♻️ refactor(auth_skeleton): modified styles and structure of auth skeleton by wrapping card inside div element to add side padding for small screens

--- src/_features/Auth/_subComponents/AuthSkeleton.tsx
+++ src/_features/Auth/_subComponents/AuthSkeleton.tsx
@@ -12,27 +12,29 @@ interface AuthSkeletonProps {
 
 function AuthSkeleton({ isLogin = false }: AuthSkeletonProps) {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader className="">
-        <Skeleton className="bg-primary-50 mx-auto h-4 w-1/2" />
+    <div className="w-full max-w-sm px-4">
+      <Card className="">
+        <CardHeader className="">
+          <Skeleton className="bg-primary-50 mx-auto h-4 w-1/2" />
+          <Skeleton className="bg-primary-50 mx-auto h-4 w-2/3" />
+        </CardHeader>
+        <div className="mt-2 mb-4 flex w-full flex-nowrap items-center justify-center gap-3 px-6">
+          <Skeleton className="bg-primary-50 h-8 grow" />
+          <Skeleton className="bg-primary-50 h-8 grow" />
+          <Skeleton className="bg-primary-50 h-8 grow" />
+        </div>
+        <CardContent>
+          <Skeleton
+            className={`bg-primary-50 ${isLogin ? "aspect-video" : "h-72"} w-full`}
+          />
+        </CardContent>
         <Skeleton className="bg-primary-50 mx-auto h-4 w-2/3" />
-      </CardHeader>
-      <div className="mt-2 mb-4 flex w-full flex-nowrap items-center justify-center gap-3 px-6">
-        <Skeleton className="bg-primary-50 h-8 grow" />
-        <Skeleton className="bg-primary-50 h-8 grow" />
-        <Skeleton className="bg-primary-50 h-8 grow" />
-      </div>
-      <CardContent>
-        <Skeleton
-          className={`bg-primary-50 ${isLogin ? "aspect-video" : "h-72"} w-full`}
-        />
-      </CardContent>
-      <Skeleton className="bg-primary-50 mx-auto h-4 w-2/3" />
-      <CardFooter className="mt-4 flex flex-col items-center gap-4">
-        <Skeleton className="bg-primary-50 mx-auto h-4 w-1/2" />
-        <Skeleton className="bg-primary-50 mx-auto h-4 w-1/2" />
-      </CardFooter>
-    </Card>
+        <CardFooter className="mt-4 flex flex-col items-center gap-4">
+          <Skeleton className="bg-primary-50 mx-auto h-4 w-1/2" />
+          <Skeleton className="bg-primary-50 mx-auto h-4 w-1/2" />
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
 
