91edbe63241834fe1d9a203e8c35074e56d5a1ca
Mostafa Ahmed <mostafafikry97@gmail.com>
2026-04-11 13:12:10 +0200

    ✨ feat(role_based_routes): created instructor-based and admins-based routes depending on role value from user session inside proxy.ts

--- src/_utils/constants/userRoles.ts
+++ src/_utils/constants/userRoles.ts
@@ -2,4 +2,4 @@ export const userRoles = {
   student: "STUDENT",
   instructor: "INSTRUCTOR",
   admin: "ADMIN",
-};
+} as const;
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,39 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { userRoles } from "./_utils/constants/userRoles";
 
-//todo: add here the protected routes inside array
+//todo : add authenticated routes
 const isProtectedRoute = createRouteMatcher([]);
 
 const isNonAuthRoute = createRouteMatcher(["/auth(.*)", "/auth(.*)"]);
 
+const instructorRoutes = createRouteMatcher(["/coursesDashboard"]);
+
+//todo : add admin routes
+const adminRoutes = createRouteMatcher([]);
+
 export default clerkMiddleware(async (auth, req) => {
+  const userMetaData = (await auth()).sessionClaims?.userMetadata as {
+    role: string;
+  };
+  const role = userMetaData?.role as (typeof userRoles)[keyof typeof userRoles];
+
   if (isProtectedRoute(req)) {
     //* add the desired pathname to a search param called redirect_url so user is redirected to it after sign in
     await auth.protect();
   }
+
   if ((await auth()).isAuthenticated && isNonAuthRoute(req)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  //* Instructor-only route
+  if (instructorRoutes(req) && role !== userRoles.instructor) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  //* Admin-only route
+  if (adminRoutes(req) && role !== userRoles.admin) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
